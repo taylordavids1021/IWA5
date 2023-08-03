13d334ab8d5ae7af70b40fc5d3780ae97b04524d
Taylor <taylordavids997@gmail.com>
2023-08-03 10:07:19 +0200

    challenge code from lms

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+FREE_WARNING = 'Free shipping only applies to single customer orders'
+BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+NONE_SELECTED = '0'
+
+if (location = RSA) { shipping === 400 && currency === 'R' }
+
+if (location = NAM) {shipping = 600     // add curly bracket if else statement
+    } else {shipping = 800
+}
+shoes = 300 * 1
+toys = 100 * 5   // add equal sign
+shirts = 150 * 'NONE_SELECTED'
+batteries = 35 * 2   // add equal sign
+pens = 5 * 'NONE_SELECTED' 
+
+shipping = null
+currency = $
+
+if (shoes + batteries + pens + shirts > 1000 &&  ) {
+	if (location = NAM && customers < 2) {
+			if (location = RSA)
+		    shipping = 0 || calcShipping
+		}
+	}
+
+if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+
+location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+
+customers = '1'
+const location = 'RSA'
+currency = null
