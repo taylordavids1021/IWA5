54d8101eeedfde7d6a16bb2970792cfa0f3bd91c
Taylor <taylordavids997@gmail.com>
2023-08-04 16:47:05 +0200

    add comment and if else statement for customer ordering from RSA and NAM

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,43 @@
-const feeWarning = "Free shipping only applies to single customer orders";  // add const before naming convention
-const bannedWarning = "Unfortunately we do not ship to your country of residence";
-const noneSelected = 0;     // Change to numnber instead of string
+const feeWarning = "Free shipping only applies to single customer orders";  // Add const before naming convention
 
-const shipping = null
-const currency = "$"
+const bannedWarning = "Unfortunately we do not ship to your country of residence";
+const noneSelected = 0;     		// Change to numnber instead of string
+
+let shipping = null					// Add shipping price
+let currency = null					// Add currency in string
 
 const customers = 1;
-//const location = "RSA"
+let locations = "RSA" ;
 
-//if (location = "RSA") { shipping === 400 && currency === "R" }  	// put data in a string
+let shoes = 300 * 1
+let toys = 100 * 5					// Add = instead of -
+let shirts = 150 * noneSelected   	// Change string to const noneSelected
+let batteries = 35 * 2
+let pens = 5 * noneSelected
 
-// if (location = "NAM") {		// add curly bracket if else statement
-//     shipping = 600
-//     } else {shipping = 800
-// }
-
-const shoes = 300 * 1
-const toys = 100 * 5
-const shirts = 150 * noneSelected   // change string to const noneSelected
-const batteries = 35 * 2
-const pens = 5 * noneSelected
-
-if (shoes + batteries + pens + shirts >= 1000 ) {
-	if (location === "NAM" && customers <= 2) {
-			if (location === "RSA")
-		    shipping = 0 || shipping
-		}
+if (locations === "RSA") { 
+	shipping = 400 
+	currency = "R"   				// Put data in a string
+	if ( shoes + batteries + toys + pens + shirts >= 1000 && customers === 1) {
+		shipping = 0;
+	}}
+	else if (locations === "NAM") { 
+		shipping = 600
+		currency = "$" 
+	if ( shoes + batteries + toys + pens + shirts >= 60 && customers === 1) {
+		shipping = 0;
+	}} 
+	else { 
+		shipping = 800
+		currency = "$" 
 	}
 	
-if (shipping === 0 || customers === 1) { console.log(feeWarning) }
+if (shipping === 0 && customers !== 1) { console.log(feeWarning) }
 
-//if ( location = "NK") {console.log(feeWarning)} //: console.log("price", currency, shoes + batteries + pens + shirts + shipping)
- location === "RSA" ? console.log(bannedWarning): console.log("price", currency, shoes + batteries + pens + shirts + shipping)
+// Console.log message 
+
+locations = "NK" ? console.log(bannedWarning) : console.log("Price :", currency, shoes + batteries + pens + shirts + toys + shipping)
+
+// if (locations = "NAM" ) {console.log("Price :", currency, shoes + batteries + pens + shirts + toys + shipping)}
+
+if (locations = "RSA" ) {console.log("Price :", currency, shoes + batteries + pens + shirts + toys + shipping)}
